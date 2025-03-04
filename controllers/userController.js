049c1b7d63f1121139f3292f6a4b133d02f21678
mobin <mobinmathew888@gmail.com>
2025-03-04 21:56:04 +0530

    Saving Image Name To DataBase

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,9 +57,6 @@ exports.getMe = (req, res, next) => {
 
 // 2) UPDATING THE CURRENT USER DATA.
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
-
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -71,7 +68,10 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   // 2) Filtered out unwanted fields names that are not allowed to be updated.
   const filteredBody = filterObj(req.body, 'name', 'email');
-
+  
+  // if user update photo then assign a photo property to filteredBody
+  if (req.file) filteredBody.photo = req.file.filename;
+  
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
