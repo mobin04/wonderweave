77d3433d8874a46b74def01e8ea842b54a623a03
mobin <mobinmathew888@gmail.com>
2025-03-07 11:21:51 +0530

    Implemented A Complex Email Class

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13034,7 +13034,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57141" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54211" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- utils/email.js
+++ utils/email.js
@@ -1,29 +1,66 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) create a transporter.
-  const transporter = nodemailer.createTransport({
-    // service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+// new Email(user, url).sendWelcome();
 
-  // 2) Define the email options.
-  const mailOptions = {
-    from: 'Natour <natours.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0]; // Extract first name from full name
+    this.url = url;  // URL (e.g., for password reset, confirmation, etc.)
+    this.from = `Natour <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 3) Actually send the email.
-  await transporter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      // 1) create a transporter.
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email.
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template.
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName, // Pass first name to template
+      url: this.url, // Pass URL to template
+      subject // Pass subject to template
+    });
+
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from, // Sender email
+      to: this.to, // Recipient email
+      subject: subject, // Email subject
+      html: html, // Rendered HTML content
+      text: htmlToText.fromString(html) // Convert HTML to plain text
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  // Helper method to send a welcome email
+  async sendWelcome() {
+   await this.send('welcome', 'Welcome to Natours.');
+  }
 };
 
-module.exports = sendEmail;
+// const sendEmail = async (options) => {
+//   // 2) Define the email options.
+
+//   // 3) Actually send the email.
+//   await transporter.sendMail(mailOptions);
+// };
